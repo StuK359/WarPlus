96a99676ff1701411e977ae046a07a5fd1bbbdbd
Stuart Klingman <stuartk@avatek.com>
2021-03-09 13:56:04 -0800

    Cleaning up for new deploy test, with sound.

--- js/mainWarPlus.js
+++ js/mainWarPlus.js
@@ -16,9 +16,7 @@ renderDeckInContainer(masterDeck, document.getElementById('master-deck-container
 let shuffledDeck;
 
 const  gameState, winner, roundScore, gameScore;
-const Player1AmmoStack; Player2AmmoStack, Player1WinStack, Player2WinStack, 
-TieCardsStack;
-
+// const Player1AmmoStack; Player2AmmoStack, Player1WinStack, Player2WinStack, TieCardsStack;
 
 
 /*----- cached element references -----*/
@@ -42,9 +40,9 @@ document.querySelector('button').addEventListener('click', renderShuffledDeck);
 document.querySelector('button').addEventListener('loadstart', playStartupSound);
 
 /*----- functions -----*/
-function mySleep(ms) {
-   return new Promise(resolve => setTimeout(resolve, ms || DEFAULT_DELAY));
-};
+// function mySleep(ms) {
+//    return new Promise(resolve => setTimeout(resolve, ms || DEFAULT_DELAY));
+// };
 
 function renderShuffledDeck() {
   // Create a copy of the masterDeck (leave masterDeck untouched!)
@@ -106,100 +104,100 @@ function buildMasterDeck() {
   return deck;
 }
 
-function initializeGame() {
-   console.log("\nGame Initialized.");
-};
+// function initializeGame() {
+//    console.log("\nGame Initialized.");
+// };
 
-renderGame(); {
-   console.log("\nGame Rendered. ");
-};
+// renderGame(); {
+//    console.log("\nGame Rendered. ");
+// };
 
-function shuffleDeck() {
-    console.log("\nCards shuffled.");
-};
+// function shuffleDeck() {
+//     console.log("\nCards shuffled.");
+// };
 
-function splitDeck() {
-    console.log("\nCard deck has been split into two randomized stacks of cards, called Ammo Piles.")
-};
+// function splitDeck() {
+//     console.log("\nCard deck has been split into two randomized stacks of cards, called Ammo Piles.")
+// };
 
-function showPayer1AmmoPile() {
-    console.log("\nPlayer 1's Ammo Pile Visible.");
-};
+// function showPayer1AmmoPile() {
+//     console.log("\nPlayer 1's Ammo Pile Visible.");
+// };
 
-function hidePlayer1AmmoPile() {
-    console.log("\nPlayer 1's Ammo Pile Hidden. ");
-};
+// function hidePlayer1AmmoPile() {
+//     console.log("\nPlayer 1's Ammo Pile Hidden. ");
+// };
 
-function showPlayer2AmmoPile() {
-    console.log("\nPlayer 2's Ammp Pile Visible. ");
-};
+// function showPlayer2AmmoPile() {
+//     console.log("\nPlayer 2's Ammp Pile Visible. ");
+// };
 
-function hidePlayer2AmmoPile() {
-    console.log("\nPlayer 2's Ammo Pile Hidden. ");
-};
+// function hidePlayer2AmmoPile() {
+//     console.log("\nPlayer 2's Ammo Pile Hidden. ");
+// };
 
-function showPlayer1WinPile() {
-    console.log("\nPlayer 1's Win Pile Visible.");
-};
+// function showPlayer1WinPile() {
+//     console.log("\nPlayer 1's Win Pile Visible.");
+// };
 
-function hidePlayer1WinPile() {
-    console.log("\nPlayer 1's Win Pile Hidden. ");
-};
+// function hidePlayer1WinPile() {
+//     console.log("\nPlayer 1's Win Pile Hidden. ");
+// };
 
-function showPlayer2WinPile() {
-    console.log("\nPlayer 2's Win Pile Visible. ");
-};
+// function showPlayer2WinPile() {
+//     console.log("\nPlayer 2's Win Pile Visible. ");
+// };
 
-function hidePlayer2WinPile() {
-    console.log("\nPlayer 2's Win Pile Hidden. ");
-};
+// function hidePlayer2WinPile() {
+//     console.log("\nPlayer 2's Win Pile Hidden. ");
+// };
 
-function playPlayer1CardFaceUp() {
-    console.log("\nPlayer 1's next card is face up. ");
-};
+// function playPlayer1CardFaceUp() {
+//     console.log("\nPlayer 1's next card is face up. ");
+// };
 
-function playPlayer2CardFaceUp() {
-    console.log("Player 2's next card is face up");
-};
+// function playPlayer2CardFaceUp() {
+//     console.log("Player 2's next card is face up");
+// };
 
-function playPlayer1CardFaceUpFlipAnimation() {
-    console.log("\nPlayer 1's next card is face up with STYLE!");
-};
+// function playPlayer1CardFaceUpFlipAnimation() {
+//     console.log("\nPlayer 1's next card is face up with STYLE!");
+// };
 
-function playPlayer2CardFaceUpFlipAnimation() {
-    console.log("\nPlayer 2's next card is face up with STYLE!");
-};
+// function playPlayer2CardFaceUpFlipAnimation() {
+//     console.log("\nPlayer 2's next card is face up with STYLE!");
+// };
 
-function compareCards() {
-    console.log("\nCards have been compared. Either Player 1 or Player 2 won, or it was a tie. ");
-};
+// function compareCards() {
+//     console.log("\nCards have been compared. Either Player 1 or Player 2 won, or it was a tie. ");
+// };
 
-function moveTieCardsAside() {
-    console.log("\nTie cards have been slid left to make room for more cards. ");
-};
+// function moveTieCardsAside() {
+//     console.log("\nTie cards have been slid left to make room for more cards. ");
+// };
 
-function playPlayer1CardFaceDown() {
-    console.log("\nPlayer 1 played a facedown card due to a tie.");
-};
+// function playPlayer1CardFaceDown() {
+//     console.log("\nPlayer 1 played a facedown card due to a tie.");
+// };
 
-function playPlayer2CardFaceDown() {
-    console.log("\nPlayer 2 played a facedown card due to a tie.");
-};
+// function playPlayer2CardFaceDown() {
+//     console.log("\nPlayer 2 played a facedown card due to a tie.");
+// };
 
-function moveCardsToPlayer1WinPile() {
-    console.log("\nPlayer 1 won! Moved cards to his Win pile.");
-};
+// function moveCardsToPlayer1WinPile() {
+//     console.log("\nPlayer 1 won! Moved cards to his Win pile.");
+// };
 
-function moveCardsToPlayer2WinPile() {
-    console.log("\nPlayer 2 won! Moved cards to his Win pile. ");
-};
+// function moveCardsToPlayer2WinPile() {
+//     console.log("\nPlayer 2 won! Moved cards to his Win pile. ");
+// };
 
-function movePlayer1WinPileToAmmoPile() {
-    console.log("\nPlayer 1 ran out of cards! Move his Win pile over to his Ammo pile.");
-};
+// function movePlayer1WinPileToAmmoPile() {
+//     console.log("\nPlayer 1 ran out of cards! Move his Win pile over to his Ammo pile.");
+// };
 
-function movePlayer2WinPileToAmmoPile() {
-    console.log("\nPlayer 2 ran out of cards! Move his Win pile over to his Ammo pile. ");
-};
+// function movePlayer2WinPileToAmmoPile() {
+//     console.log("\nPlayer 2 ran out of cards! Move his Win pile over to his Ammo pile. ");
+// };
 
 
